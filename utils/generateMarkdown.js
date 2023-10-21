0bf00279a27c9de5099507789b2cb67a1d6a17fa
Maggie McCausland <maggiemccausland@hotmail.com>
2023-10-21 17:03:46 -0600

    changed readme file layout, commented out function not in use, deleted test readme file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -60,11 +60,13 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title}   
+  ${renderLicenseBadge(data.license)}
+  
   ## Table of Contents
   - [Description](#description)
   - [License](#license)
@@ -79,16 +81,15 @@ function generateMarkdown(data) {
 
   ## License
   ${renderLicenseBadge(data.license)}  
-  This project is licensed under: ${data.license}
-  ${renderLicenseLink(data.license)}
+  This project is licensed under the ${renderLicenseLink(data.license)} license.
 
   ## Installation
-  To install necessary dependencies, run the following command:
-  > ${data.installation}
+  To install necessary dependencies, run the following command:  
+  *${data.installation}*
 
   ## Testing
-  To run tests, run the following command:
-  > ${data.testing}
+  To run tests, run the following command:  
+  *${data.testing}*
 
   ## Usage
   ${data.usage}
@@ -97,8 +98,8 @@ function generateMarkdown(data) {
   ${data.contributing}
 
   ## Questions
-  - To view more of my work, visit my GitHub page: [${data.username} GitHub](https://github.com/${data.username})
-  - If you have any questions, please feel free to contact me by email: ${data.email}
+  To view more of my work, visit my GitHub page: [${data.username} GitHub](https://github.com/${data.username})  
+  If you have any questions, please feel free to contact me by email: ${data.email}
 `;
 }
 
